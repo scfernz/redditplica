26782afb0fc278ad14aea48df505a3e2c3a0a97f
Sean Fernandez <scfernz@gmail.com>
2016-08-21 00:03:31 -0700

    Initial Angular front-end

--- app.js
+++ app.js
@@ -2,5 +2,22 @@ angular.module('redditplica', [])
 .controller('mainCtrl', [
   '$scope',
   function($scope){
-    $scope.test = "Hello World"
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 8},
+      {title: 'post 3', upvotes: 12},
+      {title: 'post 4', upvotes: 94},
+      {title: 'post 5', upvotes: 8}
+    ];
+    $scope.addPost = function() {
+      if (!$scope.title || $scope.title === '') {return;}
+      else{
+        $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+        $scope.title = '';
+        $scope.link = '';
+      }
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
   }]);
