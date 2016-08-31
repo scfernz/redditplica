b156c1cbfcae28da34915ea31a478920016ddf4e
Sean Fernandez <scfernz@gmail.com>
2016-08-31 00:27:20 -0700

    Formatting

--- app.js
+++ app.js
@@ -40,17 +40,17 @@ angular.module('redditplica', ['ui.router'])
         {author: 'Joe', body: 'Cool post!', upvotes: 0},
         {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
       ]
-    });
-    $scope.post = post.posts[$stateParams.id];
-    $scope.addComment = function() {
-      if($scope.body === '') { return;}
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = '';
-    }
+    }),
+    $scope.post = post.posts[$stateParams.id]
+    // $scope.addComment = function() {
+    //   if($scope.body === '') { return;}
+    //   $scope.post.comments.push({
+    //     body: $scope.body,
+    //     author: 'user',
+    //     upvotes: 0
+    //   });
+    //   $scope.body = '';
+    // }
 }])
 
 .controller('MainCtrl', [
