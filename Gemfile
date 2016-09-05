dd39acd539e86e59ab3df43aae7e3307f5b1044e
Sean Fernandez <scfernz@gmail.com>
2016-09-05 10:43:45 -0700

    Modularized js files

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 # angular
 gem 'angular-rails-templates'
+gem 'ngannotate-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,64 +9,11 @@ angular.module('redditplica', ['ui.router', 'templates'])
         url: '/home',
         templateUrl: 'home/_home.html',
         controller: 'MainCtrl'
-      });
-
-    $stateProvider
+      })
       .state('posts', {
         url: '/posts/{id}',
         templateUrl: 'posts/_posts.html',
         controller: 'PostsCtrl'
       });
     $urlRouterProvider.otherwise('home');
-}])
-
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-
-.controller('PostsCtrl', [
-  '$scope',
-  '$stateParams',
-  'posts',
-  function($scope, $stateParams, posts) {
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-      ]
-    }),
-    $scope.post = post.posts[$stateParams.id],
-    $scope.addComment = function() {
-      if($scope.body === '') { return;}
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = '';
-    }
-}])
-
-.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function($scope, posts){
-    $scope.posts = posts.posts;
-    $scope.addPost = function() {
-      if (!$scope.title || $scope.title === '') {return;}
-      else{
-        $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
-        $scope.title = '';
-        $scope.link = '';
-      }
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    }
-  }]);
+}]);
