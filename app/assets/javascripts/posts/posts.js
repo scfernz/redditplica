5f9f04bb49cc95e3042c2709602877f61d4690e3
Sean Fernandez <scfernz@gmail.com>
2016-09-05 11:16:21 -0700

    Modularized angular files

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,13 +1,5 @@
 angular.module('redditplica')
 
-// $scope.posts = [
-//   {title: 'post 1', upvotes: 5},
-//   {title: 'post 2', upvotes: 2},
-//   {title: 'post 3', upvotes: 15},
-//   {title: 'post 4', upvotes: 9},
-//   {title: 'post 5', upvotes: 4}
-// ];
-
 .factory('posts', [function(){
   var obj = {
     posts: []
--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,9 +1,7 @@
 angular.module('redditplica')
 
 .controller('PostsCtrl', [
-  '$scope',
-  '$stateParams',
-  'posts',
+  '$scope', '$stateParams', 'posts',
   function($scope, $stateParams, posts) {
     $scope.posts.push({
       title: $scope.title,
@@ -13,15 +11,15 @@ angular.module('redditplica')
         {author: 'Joe', body: 'Cool post!', upvotes: 0},
         {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
       ]
-    })
-    // $scope.post = post.posts[$stateParams.id],
-    // $scope.addComment = function() {
-    //   if($scope.body === '') { return;}
-    //   $scope.post.comments.push({
-    //     body: $scope.body,
-    //     author: 'user',
-    //     upvotes: 0
-    //   });
-    //   $scope.body = '';
-    // }
+    }),
+    $scope.post = post.posts[$stateParams.id],
+    $scope.addComment = function() {
+      if($scope.body === '') { return;}
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    }
 }])
