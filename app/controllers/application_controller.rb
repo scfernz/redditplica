a5f3a6d94b19eb3081ecebd5ac8e25116a4d548a
Sean Fernandez <scfernz@gmail.com>
2016-09-26 23:56:07 -0700

    Added authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   respond_to :json
+
   def angular
     render 'layouts/application'
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
